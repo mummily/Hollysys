994a53e664a5bdb8c0bd272331dd5561203e8f77
wangbin184035 <wangbin184035@hollysys.net>
2020-11-16 14:19:48 +0800

    添加转C示例

---
+++ 6. M7预研/转C/LKX转C文件/iec-c/s1234567.c
@@ -0,0 +1,8 @@
+#include"s1234567.h"
+#include"HOLLYSYS-M7-AT-TASK-DEFINE.h"
+void  s1234567 (  ) 
+{ 
+ (*( int16_t*)( gl_ulDatRangeHeadAdress +H_GrVarHAdr+H__g1))  
+  =12345; 
+
+}
---
+++ 6. M7预研/转C/LKX转C文件/iec-c/s1234567.h
@@ -0,0 +1,11 @@
+#ifndef  __S1234567_H_ 
+   
+#define __S1234567_H_ 
+  
+#include<stdint.h> 
+#include<stdbool.h> 
+
+
+void  s1234567 (  ) ;
+#endif //__S1234567_H_ 
+  
