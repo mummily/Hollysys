957cef2cf68e413dbf5b145a3b218078a7752dc7
wangbin184035 <wangbin184035@hollysys.net>
2020-11-27 15:35:00 +0800

    添加一个简单加法的实现

---
+++ 6. M7预研/转C/WB转C文件/fun/aaa.h
@@ -0,0 +1,16 @@
+#ifndef __AAA_H_
+#define __AAA_H_
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <math.h>
+
+#pragma pack(1)
+struct aaa
+{
+int16_t* a;
+int16_t* b;
+int16_t* c;
+};
+#pragma pack()
+#endif
