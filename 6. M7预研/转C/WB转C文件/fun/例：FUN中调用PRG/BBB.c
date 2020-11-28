8632301d0d61416ad455dde897e8e52550325682
wangbin184035 <wangbin184035@hollysys.net>
2020-11-28 14:28:49 +0800

    FUN中调用PRG

---
+++ 6. M7预研/转C/WB转C文件/fun/例：FUN中调用PRG/BBB.c
@@ -0,0 +1,13 @@
+#include "BBB.h"
+#include "HOLLYSYS-M7-AT-TASK-DEFINE.h"
+#include "AAA.h"
+
+bool BBB(void* _pHead) {
+bool BBB = false;
+struct BBB* pHead = (struct BBB*)_pHead;
+bool __AT__BBB_EN_1 = true;
+
+if((bool)__AT__BBB_EN_1) { 
+AAA(); }
+return BBB;
+}
---
+++ 6. M7预研/转C/WB转C文件/fun/例：FUN中调用PRG/BBB.h
@@ -0,0 +1,16 @@
+#ifndef __BBB_H_
+#define __BBB_H_
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <math.h>
+
+// Fun
+#pragma pack(1)
+struct BBB
+{
+// IN
+// OUT
+};
+#pragma pack()
+#endif
