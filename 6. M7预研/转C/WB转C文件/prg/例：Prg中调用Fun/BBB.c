270a4ba1450c1bc29db54f9f3fa2e740854a6dda
wangbin184035 <wangbin184035@hollysys.net>
2020-11-28 17:04:38 +0800

    修改PRG中调FUN的实现

--- 6. M7预研/转C/WB转C文件/prg/例：Prg中调用Fun/BBB.c
+++ 6. M7预研/转C/WB转C文件/prg/例：Prg中调用Fun/BBB.c
@@ -7,7 +7,10 @@ bool __AT__BBB_EN_1 = true;
 bool __AT__BBB_1 = false;
 if((bool)__AT__BBB_EN_1) { 
 struct AAA AAA1;
-AAA1.p1 = (*(pHead->P1));
+AAA1.p1 = (*(int16_t*)(gl_ulDatRangeHeadAdress + H_GrVarHAdr + H_BBB_A));
+AAA1.p2 = (*(int16_t*)(gl_ulDatRangeHeadAdress + H_GrVarHAdr + H_BBB_B));
+AAA1.p3 = (*(int16_t*)(gl_ulDatRangeHeadAdress + H_GrVarHAdr + H_BBB_C));
 __AT__BBB_1 = AAA(&AAA1); }
-if((bool)__AT__BBB_EN_1) { (*(int16_t*)(gl_ulDatRangeHeadAdress + H_GrVarHAdr + H_BBB_C)) = __AT__BBB_1;; }
+if((bool)__AT__BBB_EN_1) { (*(int16_t*)(gl_ulDatRangeHeadAdress + H_GrVarHAdr + H_BBB_C)) = AAA1.p3;; }
+if((bool)__AT__BBB_EN_1) { (*(int16_t*)(gl_ulDatRangeHeadAdress + H_GrVarHAdr + H_BBB_D)) = __AT__BBB_1;; }
 }
