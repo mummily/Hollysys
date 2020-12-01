b94993418eea0416b7dbcdde5568e87507d8d5a9
wangbin184035 <wangbin184035@hollysys.net>
2020-12-01 14:52:48 +0800

    添加PRG中调FUN使能导出示例

--- 6. M7预研/转C/WB转C文件/prg/例：Prg中调用Fun/含使能引脚/CFCPRG.c
+++ 6. M7预研/转C/WB转C文件/prg/例：Prg中调用Fun/含使能引脚/CFCPRG.c
@@ -3,13 +3,13 @@
 #include "CFCFUN.h"
 
 void CFCPRG() {
-bool __AT__CFCPRG_EN_1 = true;
+bool __AT__CFCPRG_EN_1 = (*(((int16_t*)(gl_ulDatRangeHeadAdress + H_GrVarHAdr + H_CFCPRG_FEN))));
 bool __AT__CFCPRG_1 = false;
-if((bool)__AT__CFCPRG_EN_1) { 
-struct CFCFUN CFCFUN1;
+if((bool)__AT__CFCPRG_EN_1) { struct CFCFUN CFCFUN1;
 CFCFUN1.P1 = ((int16_t*)(gl_ulDatRangeHeadAdress + H_GrVarHAdr + H_CFCPRG_A));
 CFCFUN1.P2 = ((int16_t*)(gl_ulDatRangeHeadAdress + H_GrVarHAdr + H_CFCPRG_B));
 CFCFUN1.P3 = ((int16_t*)(gl_ulDatRangeHeadAdress + H_GrVarHAdr + H_CFCPRG_C));
 __AT__CFCPRG_1 = CFCFUN(&CFCFUN1); }
 if((bool)__AT__CFCPRG_EN_1) { (*(((int16_t*)(gl_ulDatRangeHeadAdress + H_GrVarHAdr + H_CFCPRG_D)))) = __AT__CFCPRG_1;; }
+if((bool)__AT__CFCPRG_EN_1) { (*(((int16_t*)(gl_ulDatRangeHeadAdress + H_GrVarHAdr + H_CFCPRG_FENO)))) = __AT__CFCPRG_EN_1;; }
 }
---
+++ 6. M7预研/转C/WB转C文件/prg/例：Prg中调用Fun/含使能引脚/CFCPRG.h
@@ -0,0 +1,10 @@
+#ifndef __CFCPRG_H_
+#define __CFCPRG_H_
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <math.h>
+
+// Prg
+void CFCPRG();
+#endif
